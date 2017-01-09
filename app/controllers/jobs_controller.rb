1f6f36ec04ea1577e370cbc6e40e3c646ef81aac
ranty <ranty720@gmail.com>
2017-01-09 08:52:24 +0800

    title&decription

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,26 @@
+class JobsController < ApplicationController
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def index
+    @jobs = Job.all
+  end
+  def create
+    @job = Job.new(job_params)
+
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
+end
